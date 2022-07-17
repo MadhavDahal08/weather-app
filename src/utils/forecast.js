6669e0c0f0db04ff14a7acc7c4002104669b8c0a
Madhav Dahal <madhavdahal4@gmail.com>
2022-07-17 06:31:00 +0545

    Initial message

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require("request");
+
+const forecast = (longitude, latitude, callback) => {
+  const url = `https://api.darksky.net/forecast/e6af5b5feb891b272e18f5e2fc0370a6/${latitude},${longitude}?units=si`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!");
+    } else if (body.error) {
+      callback("Unable to find location");
+    } else {
+      const message = `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degress out. There is a ${body.currently.precipProbability}% chance of rain.`;
+      callback(undefined, message);
+    }
+  });
+};
+
+module.exports = forecast;
